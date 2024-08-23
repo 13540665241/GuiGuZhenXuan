02eef20ed3385a6998ab7e2f5ee906b39a60c5b0
TooFarDJY <814605664@qq.com>
2024-08-23 18:47:00 +0800

    feat: 顶部静态搭建

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -2,7 +2,11 @@
   <template v-for="(item, index) in menuList" :key="item.path">
     <!--没有子路由-->
     <template v-if="!item.children">
-      <el-menu-item v-if="!item.meta.hidden" :index="item.path" @click="goRoute">
+      <el-menu-item
+        v-if="!item.meta.hidden"
+        :index="item.path"
+        @click="goRoute"
+      >
         <el-icon>
           <component :is="item.meta.icon"></component>
         </el-icon>
@@ -14,7 +18,11 @@
 
     <!--有子路由，但只有1个-->
     <template v-if="item.children && item.children.length === 1">
-      <el-menu-item v-if="!item.children[0].meta.hidden" :index="item.children[0].path" @click="goRoute">
+      <el-menu-item
+        v-if="!item.children[0].meta.hidden"
+        :index="item.children[0].path"
+        @click="goRoute"
+      >
         <el-icon>
           <component :is="item.children[0].meta.icon"></component>
         </el-icon>
@@ -25,7 +33,10 @@
     </template>
 
     <!--有子路由，但数量大于1-->
-    <el-sub-menu v-if="item.children && item.children.length > 1" :index="item.path">
+    <el-sub-menu
+      v-if="item.children && item.children.length > 1"
+      :index="item.path"
+    >
       <el-icon>
         <component :is="item.meta.icon"></component>
       </el-icon>
@@ -47,7 +58,7 @@ defineProps(['menuList'])
 const $router = useRouter()
 // 点击菜单回调
 const goRoute = (vc: any) => {
-  //跳转路由 
+  //跳转路由
   $router.push(vc.index)
 }
 </script>
---
+++ src/layout/tabbar/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tabbar">
+    <!-- 顶部左侧 -->
+    <div class="tabbar_left">
+      <Breadcrumb></Breadcrumb>
+    </div>
+
+    <!--    &lt;!&ndash;顶部右侧-->
+    <div class="tabbar_right">
+      <Setting></Setting>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Breadcrumb from './breadcrumb/index.vue'
+import Setting from './setting/index.vue'
+</script>
+
+<style lang="scss" scoped>
+.tabbar {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  // 盒子左右分布
+  justify-content: space-between;
+  background-image: linear-gradient(to right, #788dd3, #9ed1c7);
+
+  .tabbar_left {
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+  }
+
+  .tabbar_right {
+    display: flex;
+    align-items: center;
+    margin-right: 20px;
+  }
+}
+</style>
